c699a1104f1fc66ab598db7d12b80a4e383f1000
Jordan Kline <jordan.kline@gmail.com>
2018-03-30 10:43:36 -0400

    added controller actions and routes to garden

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,41 @@
 class GardensController < ApplicationController
+	before_action :set_garden, except: [:new, :create, :index]
+
+	def new
+		@garden = Garden.new
+	end
+
+	def create
+		@garden = Garden.new(garden_params)
+
+		if @garden.save
+			redirect_to @garden
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def index
+		@gardens = Garden.all
+	end
+
+	private
+
+	def set_garden
+		@garden = Garden.find(params[:id])
+	end
+
+	def garden_params
+		params.require(:garden).permit(:name)
+	end
+
 end
