cc711a372d9e4d30085199d2754376a42364a938
Jordan Kline <jordan.kline@gmail.com>
2018-04-11 16:24:36 -0700

    added validations to devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable, 
   # :recoverable, :rememberable, :trackable, :validatable, 
   devise :database_authenticatable, :registerable
-
+  validates :email, presence: true
+  validates :password, presence: true
+  
   has_many :gardens
   has_many :plants, through: :gardens
 
