625012fd8d7c8aac47513a8042e72a53e9e18e6f
Jordan Kline <jordan.kline@gmail.com>
2018-07-25 14:06:46 -0700

    refactored css for plants show and added an array for plant js data

--- app/assets/javascripts/plants.js
+++ app/assets/javascripts/plants.js
@@ -1,50 +1,37 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-// class Plant {
-//   constructor(obj) {
-// 	this.name = obj.name,
-// 	this.genus = obj.genus,
-// 	this.water_frequency = obj.water_frequency,
-// 	this.image_url = obj.image_url
-// 	plants.push(this);
-//   }
+let plants = [];
 
-//   dataBlob() {
-// 	return     `<div><img class="image_border" src='${this.image_url}' width="200px" height="200px"></div>
-// 			<br><div><em><strong>Name:</strong>${this.name}</em></div>
-// 			    <div><em><strong>Watering Frequency:</strong> ${this.water_frequency}</em></div>
-// 				<div><em><strong>Genus:</strong> ${this.genus}</em></div>`;
-//   }
-// }
-
-function Plant(obj) {
+class Plant {
+  constructor(obj) {
 	this.name = obj.name,
 	this.genus = obj.genus,
 	this.water_frequency = obj.water_frequency,
 	this.image_url = obj.image_url
+	plants.push(this);
+  }	
 }
+// function Plant(obj) {
+// 	this.name = obj.name,
+// 	this.genus = obj.genus,
+// 	this.water_frequency = obj.water_frequency,
+// 	this.image_url = obj.image_url
+// }
 
 Plant.prototype.dataBlobPic = function() {
-	return     `<div>
-					<img class="image_border" src='${this.image_url}' width="200px" height="200px">
+		return `<div>
+					<img class="viewer" src='${this.image_url}'>
 				</div>`;
 }
 
 Plant.prototype.dataBlobInfo = function() {
-	return     `<div id="plant-info"><br><br><br>
+		return `<div id="plant-info"><br><br><br>
 					<div><em><strong>Name: </strong>${this.name}</em></div><br />
 			    	<div><em><strong>Watering Frequency:</strong> Every ${this.water_frequency}</em> Days</div><br />
 					<div><em><strong>Genus:</strong> ${this.genus}</em></div>
 				</div>`;
 }
-
-// Plant.prototype.lineFormat = function() {
-// 	return     `<div ><div><img class="image_border" src='${this.image_url}' width="200px" height="200px"></div>
-// 			<br><div ><div><em><strong>Name:</strong> ${this.name}</em></div>
-// 			    <div><em><strong>Watering Frequency:</strong> ${this.water_frequency}</em></div>
-// 				<div><em><strong>Genus:</strong> ${this.genus}</em></div>`;
-// }
 
 function bindPlantsListeners() {
 	console.log('plants listener')
